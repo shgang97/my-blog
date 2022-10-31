87838617381ec3ce8e9f4493a9352abf9a9486fc
shgang <shgang97@163.com>
2022-11-01 01:58:32 +0800

    remove unused ajax.get

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,11 +6,6 @@
 import Index from '@/view/Index';
 export default {
   name: 'App',
-  created: function () {
-    this.axios.get('http://localhost:8081/').then((res) => {
-      console.log(res);
-    });
-  },
   components: {
     Index
   }
