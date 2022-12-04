40b5b4451a322d35ae7c1e6feadbcd59296109b0
shgang <shgang97@163.com>
2022-12-04 20:14:54 +0800

    modify router of backend

--- backend/router/router.go
+++ backend/router/router.go
@@ -19,15 +19,20 @@ func Router() {
 	router := gin.New()
 	router.Use(middleware.Cors())
 
+	// TODO 不需要登录路由
 	unauthRouter := router.Group(ContextPath)
-	unauthRouter.POST("/login", login.Login)
 
+	// TODO 需要登录路由
 	authRouter := router.Group(ContextPath)
 	authRouter.Use(middleware.JWT())
+
+	unauthRouter.POST("/login", login.Login)
+
 	userRouter := authRouter.Group("/user")
 	{
 		userRouter.GET("/get", user.UserGet)
 		userRouter.POST("/post", user.UserPost)
 	}
+
 	router.Run(":8080")
 }
