8edf6348adfc9121bd46d451395897d2fae814cb
shgang97 <shgang97@163>
2022-12-08 16:13:34 +0800

    content sub components

---
+++ frontend/src/components/Articles.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="article-list" v-for="i in 4">
+      <ArticleCard></ArticleCard>
+    </div>
+    <div>
+      <Pagination></Pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+import ArticleCard from './ArticleCard.vue'
+import Pagination from './Pagination.vue'
+export default {
+  name: "Articles",
+  components: {
+    ArticleCard,
+    Pagination
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
