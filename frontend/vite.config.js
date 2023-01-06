fc9f45a5276332c540d501436389a268863c364f
shgang <shgang97@163.com>
2023-01-06 15:34:54 +0800

    【frontend】add tip plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,5 @@
 export default {
     optimizeDeps: {
-        include: ['@kangc/v-md-editor/lib/theme/github.js',
-        '@kangc/v-md-editor/lib/plugins/tip/index.js'],
+        include: ['@kangc/v-md-editor/lib/theme/github.js', '@kangc/v-md-editor/lib/plugins/tip/index.js'],
     },
 };
